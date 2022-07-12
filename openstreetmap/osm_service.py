23722d1a2fc7b4a6740d52d3936092a896e42a10
samueljohnsegun148 <samueljohnsegun148@gmail.com>
2022-07-12 22:24:44 +0100

    add file

--- openstreetmap/osm_service.py
+++ openstreetmap/osm_service.py
@@ -238,11 +238,12 @@ def allot_intersection(processed_OSM_data, inters_rec_up
 def get_amenities(bbox_coord):
     # Send request to OSM to get amenities which are part of
     # points of interest (POIs)
-    api = overpy.Overpass(
-        url="https://pegasus.cim.mcgill.ca/overpass/api/interpreter?")
+    
     lat_min, lon_min = bbox_coord[0], bbox_coord[1]
     lat_max, lon_max = bbox_coord[2], bbox_coord[3]
     try:
+        api = overpy.Overpass(
+        url="https://pegasus.cim.mcgill.ca/overpass/api/interpreter?")
         amenities = api.query(
             f"""
         node({lat_min},{lon_min},{lat_max},{lon_max}) ["amenity"];
