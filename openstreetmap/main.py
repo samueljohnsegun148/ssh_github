13c6cb9ac5271353417ea89649cb198fc9e996da
samueljohnsegun148 <samueljohnsegun148@gmail.com>
2022-07-13 13:37:09 +0100

    add file

--- openstreetmap/main.py
+++ openstreetmap/main.py
@@ -131,8 +131,7 @@ def get_map_data():
     if validated is not None:
         return validated
     logging.debug("Sending response")
-    y={"bbox":bbox_coordinates}
-    return y
+    return response
 
 
 if __name__ == "__main__":
