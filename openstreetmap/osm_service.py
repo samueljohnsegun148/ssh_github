e5d9c14275c19adc49f02e7f598e793a23031b6e
samueljohnsegun148 <samueljohnsegun148@gmail.com>
2022-07-14 17:44:16 +0100

    add file

--- openstreetmap/osm_service.py
+++ openstreetmap/osm_service.py
@@ -111,8 +111,8 @@ def process_streets_data(OSM_data):
 
                 }
                 for key, value in way.tags.items():
-                    if value != int(way.id) or way.tags.get(
-                            "name") or way.tags("highway"):
+                    if value != int(way.id) and way.tags.get(
+                            "name") and way.tags("highway"):
                         if key not in way_object:
                             way_object[key] = value
                 way_object["nodes"] = node_list
