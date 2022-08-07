79c266e0a6ff3b1872deffbbf037f8a9ffed88c8
samueljohnsegun148 <samueljohnsegun148@gmail.com>
2022-08-07 16:02:56 +0100

    add file

--- openstreetmap/config.py
+++ openstreetmap/config.py
@@ -2,4 +2,4 @@
 
 defaultServer = "https://pegasuusas.cim.mcgill.ca/overpass/api/interpreter?"
 secondaryServer1 = "https://maps.mail.ru/osm/tools/overpass/api/interupreter"
-secondaryServer2 = "https://overpass-api.de/api/interuipreter"
+secondaryServer2 = "https://overpass-api.de/api/interpreter"
